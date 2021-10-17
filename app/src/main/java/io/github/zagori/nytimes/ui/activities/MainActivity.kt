27a2abd1169517f9b039342b0d54bc98fb1427c3
yousseflabihi <you.labihi@gmail.com>
2021-10-17 10:46:03 +0800

    Set up the navigation logic in the main activity and update kotlin and gradle plugins

--- app/src/main/java/io/github/zagori/nytimes/ui/activities/MainActivity.kt
+++ app/src/main/java/io/github/zagori/nytimes/ui/activities/MainActivity.kt
@@ -3,16 +3,17 @@ package io.github.zagori.nytimes.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import io.github.zagori.nytimes.R
+import io.github.zagori.nytimes.databinding.ActivityMainBinding
 import io.github.zagori.nytimes.viewmodels.ArticlesViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: ArticlesViewModel by viewModels()
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         viewModel.getRemoteMostViewed()
     }
